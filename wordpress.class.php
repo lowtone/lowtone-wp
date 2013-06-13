3168b560fbae5bf68a68c7a6203c51e456124834
Paul van der Meijs <paul@lowtone.nl>
2013-06-13 14:57:44 +0200

    Updated comments.

--- wordpress.class.php
+++ wordpress.class.php
@@ -1,6 +1,13 @@
 <?php
 namespace lowtone\wp;
 
+/**
+ * @author Paul van der Meijs <code@paulvandermeijs.nl>
+ * @copyright Copyright (c) 2013, Paul van der Meijs
+ * @license http://wordpress.lowtone.nl/license/
+ * @version 1.1
+ * @package wordpress\libs\lowtone\wp
+ */
 class WordPress {
 
 	public function query() {
