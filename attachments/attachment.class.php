87e00091f235adab7b01898fe9a52e0e0a1e1c57
Paul van der Meijs <paul@lowtone.nl>
2013-06-26 22:53:30 +0200

    Fixed Attachment::fromURL() and Attachment::fromFile().

--- attachments/attachment.class.php
+++ attachments/attachment.class.php
@@ -3,7 +3,8 @@ namespace lowtone\wp\attachments;
 use lowtone\net\URL,
 	lowtone\io\File,
 	lowtone\wp\posts\Post,
-	lowtone\types\datetime\DateTime;
+	lowtone\types\datetime\DateTime,
+	lowtone\util\options\Options;
 
 /**
  * @author Paul van der Meijs <code@lowtone.nl>
@@ -66,16 +67,66 @@ class Attachment extends Post {
 		return "lowtone\\wp\\attachments\\out\\AttachmentDocument";
 	}
 
-	public static function fromUrl($url) {
-		$file = File::get($url);
-
-		return static::fromFile($file);
+	public static function fromUrl($url, $options = NULL) {
+		return static::fromFile(File::get($url), $options);
 	}
 
-	public static function fromFile(File $file) {
-		$uploadDir = wp_upload_dir(DateTime::now()->format("Y/m"));
+	/**
+	 * Create and insert an attachment from a File instance.
+	 *
+	 * If the target option is defined it should point to a location inside the 
+	 * uploads base dir and also the path to the uploads base dir should use the 
+	 * exact same slashes as the value returned by wp_upload_dir or else 
+	 * _wp_relative_upload_path() will fail when the attachment meta data is 
+	 * generated.
+	 * 
+	 * @param File $file The file used to create an attachment.
+	 * @param array|NULL $options Additional options used to save the file and 
+	 * create the attachment.
+	 * @return Attachment Returns a new attachment instance.
+	 */
+	public static function fromFile(File $file, $options = NULL) {
+		$options = Options::defaults(array(
+				"target" => function() use ($file) {
+					$uploadDir = wp_upload_dir(DateTime::now()->format("Y/m"));
 
-		$file->put($uploadDir["path"] . DIRECTORY_SEPARATOR . basename($file->url()->{URL::PROPERTY_PATH}));
+					return $uploadDir["path"] . "/" . basename($file->url()->{URL::PROPERTY_PATH});
+				},
+				"defaults" => array(),
+				"post_id" => NULL,
+			), $options);
+
+		// Save file
+
+		$file = $file->put($options["target"]);
+
+		// Attachment properties
+
+		$url = site_url(str_replace("\\", "/", $file->relPath()));
+
+		$fileType = wp_check_filetype($file->url(), null);
+
+		$attachment = array(
+			self::PROPERTY_GUID => $url,
+			self::PROPERTY_POST_MIME_TYPE => $fileType["type"],
+			self::PROPERTY_POST_TITLE => $file->url()->pathinfo(PATHINFO_FILENAME),
+			self::PROPERTY_POST_CONTENT => "",
+			self::PROPERTY_POST_STATUS => self::STATUS_INHERIT
+		);
+
+		$attachment = array_merge($attachment, (array) $options["defaults"]);
+
+		$attachment[self::PROPERTY_ID] = wp_insert_attachment($attachment, $options["target"]);
+
+		// Attachment meta
+
+		include_once ABSPATH . "/wp-admin/includes/image.php";
+
+		$attachmentMeta = wp_generate_attachment_metadata($attachment[self::PROPERTY_ID], $options["target"]);
+		
+		wp_update_attachment_metadata($attachment[self::PROPERTY_ID], $attachmentMeta);
+
+		return new Attachment($attachment);
 	}
 	
 }
